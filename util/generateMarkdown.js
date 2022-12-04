35d642802a6ac5c659c8f33a479a1b665e2782b2
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2022-12-03 20:02:27 -0500

    Update on the title

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -12,7 +12,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
   https://github.com/${data.GithubUsername}/${data.Title}
   # Description
   ${data.Description}
